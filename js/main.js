c1c3bc95cd010cc26660ccc20008ecf6e24e88ab
Wojo3214 <55576529+Wojo3214@users.noreply.github.com>
2020-09-30 08:56:20 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Showing nav
+
+let navBtn = document.querySelector(".nav-mobile-btn");
+navBtn.addEventListener("click", showNav);
+
+let closeBtn = document.querySelector(".fa-times");
+closeBtn.addEventListener("click", hideNav);
+
+function showNav(){
+    document.querySelector(".nav").setAttribute("style", "right: 0; transform: scale(1); border-radius: 0;");
+    document.querySelector(".nav-mobile-container").style.display="none";
+}
+
+function hideNav(){
+    document.querySelector(".nav").setAttribute("style", "right: -3000px; transform: scale(0); border-radius: 1000px;");
+    document.querySelector(".nav-mobile-container").style.display="block";
+}
